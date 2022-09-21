de33bfa39b5bbffea2670546d9e8351425627033
Cuong Xuan Do <xuancuong.do@r4pid.com>
2022-09-22 00:14:46 +0700

    done

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,10 +1,8 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 import web3 from "./web3";
 import lottery from "./lottery";
-import React, { Component } from "react";
 
-class App extends Component {
+class App extends React.Component {
   state = {
     manager: "",
     players: [],
@@ -12,7 +10,6 @@ class App extends Component {
     value: "",
     message: "",
   };
-
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
     const players = await lottery.methods.getPlayers().call();
@@ -20,53 +17,60 @@ class App extends Component {
 
     this.setState({ manager, players, balance });
   }
+  onClick = async () => {
+    const accounts =  await web3.eth.getAccounts()
+    await lottery.methods.pickWinner().send({
+      from : accounts[0]
+    })
+    
+    this.setState({ message : 'A winner has been chosen'})
+  }
 
   onSubmit = async (event) => {
     event.preventDefault();
 
     const accounts = await web3.eth.getAccounts();
 
-    this.setState({ message: "waitin on transaction success...... " });
+    this.setState({ message: "Waiting on transaction success..." });
 
     await lottery.methods.enter().send({
       from: accounts[0],
       value: web3.utils.toWei(this.state.value, "ether"),
     });
-    this.setState({
-      message: "u have been entered to the pot , feel free to lose ",
-    });
+
+    this.setState({ message: "You have been entered!" });
   };
+
   render() {
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p>This contract is managed by {this.state.manager}</p>
         <p>
-          There are {this.state.players.length} entered in the lottery right now
+          This contract is managed by {this.state.manager}. There are currently{" "}
+          {this.state.players.length} people entered, competing to win{" "}
+          {web3.utils.fromWei(this.state.balance, "ether")} ether!
         </p>
-        <p>
-          But only one can win the prize of{" "}
-          {web3.utils.fromWei(this.state.balance, "ether")}
-          USD from the lottery
-        </p>
-        <hr />
 
+        <hr />
         <form onSubmit={this.onSubmit}>
-          <h4>Want to try your luck</h4>
+          <h4>Want to try your luck?</h4>
           <div>
             <label>Amount of ether to enter</label>
+            <input
+              value={this.state.value}
+              onChange={(event) => this.setState({ value: event.target.value })}
+            />
           </div>
-          <input
-            value={this.state.value}
-            onChange={(event) => this.setState({ value: event.target.value })}
-          />
           <button>Enter</button>
         </form>
+
         <hr />
+
+        <h4>Ready to pick a winner?</h4>
+        <button onClick={this.onClick}>Pick a winner</button>
         <h1>{this.state.message}</h1>
       </div>
     );
   }
 }
-
 export default App;
